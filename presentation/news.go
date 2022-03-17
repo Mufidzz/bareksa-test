61e429ee590d70502ceedf0e41ee258a7a4b07f6
Mufidz Zukhruf <dmzmzzz@gmail.com>
2022-03-17 20:37:41 +0700

    create_news repository created, unit tested

--- presentation/news.go
+++ presentation/news.go
@@ -4,6 +4,11 @@ import (
 	"time"
 )
 
+// Constant for News Status
+const NEWS_STATUS_DRAFT = 1
+const NEWS_STATUS_PUBLISHED = 2
+const NEWS_STATUS_DELETED = 3
+
 type GetNewsResponse struct {
 	ID         int64     `db:"id"`
 	CreatedAt  time.Time `db:"created_at"`
@@ -12,4 +17,11 @@ type GetNewsResponse struct {
 	Content    string    `db:"content"`
 	TopicsName string    `db:"topics_name"`
 	TagsName   string    `db:"tags_name"`
+	Status     int       `db:"status"`
+}
+
+type CreateBulkNewsRequest struct {
+	Title   string `db:"title"`
+	Content string `db:"content"`
+	Status  int    `db:"status"`
 }
