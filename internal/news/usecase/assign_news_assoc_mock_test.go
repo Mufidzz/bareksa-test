828e1ba4f7c1136570cc78d1b74b2b664b5aa73d
Mufidz Zukhruf <dmzmzzz@gmail.com>
2022-03-19 16:11:19 +0700

    clean assoc implemented, unit tested

--- internal/news/usecase/assign_news_assoc_mock_test.go
+++ internal/news/usecase/assign_news_assoc_mock_test.go
@@ -5,6 +5,8 @@ import "github.com/Mufidzz/bareksa-test/presentation"
 type MockAssignNewsAssocRepository struct {
 	createBulkNewsTopicsAssoc createBulkNewsTopicsAssoc
 	createBulkNewsTagsAssoc   createBulkNewsTagsAssoc
+	cleanNewsTopicsAssoc      cleanNewsTopicsAssoc
+	cleanNewsTagsAssoc        cleanNewsTagsAssoc
 }
 
 type createBulkNewsTopicsAssoc struct {
@@ -15,9 +17,24 @@ type createBulkNewsTagsAssoc struct {
 	err error
 }
 
+type cleanNewsTopicsAssoc struct {
+	err error
+}
+
+type cleanNewsTagsAssoc struct {
+	err error
+}
+
 func (manar *MockAssignNewsAssocRepository) CreateBulkNewsTopicsAssoc(in []presentation.CreateNewsTopicsAssoc) (err error) {
 	return manar.createBulkNewsTopicsAssoc.err
 }
 func (manar *MockAssignNewsAssocRepository) CreateBulkNewsTagsAssoc(in []presentation.CreateNewsTagsAssoc) (err error) {
 	return manar.createBulkNewsTagsAssoc.err
+}
+
+func (manar *MockAssignNewsAssocRepository) CleanNewsTopicsAssoc(newsID []int) (err error) {
+	return manar.cleanNewsTopicsAssoc.err
+}
+func (manar *MockAssignNewsAssocRepository) CleanNewsTagAssoc(newsID []int) (err error) {
+	return manar.cleanNewsTagsAssoc.err
 }
